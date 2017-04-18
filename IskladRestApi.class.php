880d6fcf4f072840c589e16a9b8cf25116a1d71d
Zoltán Laca <zoltanlaca@users.noreply.github.com>
2017-04-18 23:33:11 +0200

    Update IskladRestApi.class.php

--- IskladRestApi.class.php
+++ IskladRestApi.class.php
@@ -1,28 +1,8 @@
 <?php
 /**
-* RestAPI pre Isklad
-* @author Zoltán Laca, Jr.
+* @package IskladRestApi
+* @author Zoltán Laca <zoltan.laca@fortuity.sk>
 * @version v1
-
-* With this class we have just 3 steps to send data to Isklad:
-* 1. Include this class to your php script
-*     include('IskladRestApi.class.php');
-* 2. Initialize it / test connection
-*     IskladRestApi::Initialize(AUTH_ID, AUTH_KEY, AUTH_TOKEN);
-*     (under development we can test the connection with  IskladRestApi::ConnectionTest(); )
-* 3. If ConnectionTest is OK, we call the method, we want, e.g. CreateNewOrder()
-*     IskladRestApi::CreateNewOrder($params);
-* 
-* Be sure to use ConnectionTest only under development, do not call it before each request!
-* 
-* $params is an array, which must be prepared for each method in structure described in the PDF API Manual.
-* e.g.This params is data from the real order from Prestashop (name, adress, contact, items...)
-* 
-* We can get last request, or last response, just call:
-* IskladRestApi::GetRequest();
-* IskladRestApi::GetResponse();
-* 
-* Thats all :)
 */
 
 class IskladRestApi
